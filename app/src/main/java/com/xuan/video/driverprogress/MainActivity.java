b5e48d33fd6aae3f34ba69aaad9a3118bd282c69
DZX <2994734692@qq.com>
2017-03-29 12:27:33 +0800

    删除没用的代码

--- app/src/main/java/com/xuan/video/driverprogress/MainActivity.java
+++ app/src/main/java/com/xuan/video/driverprogress/MainActivity.java
@@ -1,6 +1,6 @@
 package com.xuan.video.driverprogress;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,40 +8,79 @@ import android.widget.SeekBar;
 
 import com.xuan.driverprogress.view.DriverProgress;
 
-public class MainActivity extends Activity {
-    private DriverProgress myview;
 
-    private SeekBar seekBar;
+public class MainActivity extends AppCompatActivity {
+    private DriverProgress myView;
 
-    private Button button;
+    private SeekBar seekbar_progress,seekbar_width,seekbar_radius
+            ,seekbar_index,seekbar_num;
+
+    private Button button_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myview = (DriverProgress) findViewById(R.id.myview);
-        seekBar = (SeekBar) findViewById(R.id.seekbar);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        myView = (DriverProgress) findViewById(R.id.myview);
+        button_start = (Button) findViewById(R.id.button_start);
+
+        seekbar_progress = (SeekBar) findViewById(R.id.seekbar_progress);
+        seekbar_width = (SeekBar) findViewById(R.id.seekbar_width);
+        seekbar_radius = (SeekBar) findViewById(R.id.seekbar_radius);
+        seekbar_index = (SeekBar) findViewById(R.id.seekbar_index);
+        seekbar_num = (SeekBar) findViewById(R.id.seekbar_num);
+
+
+        /**
+         * 开始动画
+         */
+        button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myview.startAnimation();
+                myView.startAnimation();
             }
         });
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                myview.setProgress(progress);
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        seekbar_progress.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        seekbar_width.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        seekbar_radius.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        seekbar_index.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        seekbar_num.setOnSeekBarChangeListener(onSeekBarChangeListener);
     }
+
+    /**
+     * seekbar event
+     */
+    private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            switch (seekBar.getId()){
+                case R.id.seekbar_progress:
+                    myView.setProgress(progress);
+                    break;
+                case R.id.seekbar_width:
+                    myView.setPanelWidth(progress);
+                    break;
+                case R.id.seekbar_radius:
+                    myView.setPanelRadius(progress);
+                    break;
+                case R.id.seekbar_index:
+                    myView.setIndicatorRadius(progress);
+                    break;
+                case R.id.seekbar_num:
+                    myView.setPanelDensity(progress);
+                    break;
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
--- library/src/main/java/com/xuan/driverprogress/view/DriverProgress.java
+++ library/src/main/java/com/xuan/driverprogress/view/DriverProgress.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,33 +21,36 @@ import android.view.animation.OvershootInterpolator;
  * 驾驶盘进度条
  */
 
-public class DriverProgress extends View{
+public class DriverProgress extends View {
     private Paint paint;
     private Context context;
     //仪表盘半径
-    private int PANEL_RADIUS = 70;
-    private final int PANEL_RADIUS_DEFALUT = 70;
+    private int PANEL_RADIUS = 150;
+    private final int PANEL_RADIUS_DEFALUT = 150;
     //仪表盘宽度
-    private int PANEL_WIDTH = 45;
-    private final int PANEL_WIDTH_DEFAULT = 45;
+    private int PANEL_WIDTH = 90;
+    private final int PANEL_WIDTH_DEFAULT = 90;
     //刻度表的密度
     private int PANEL_DENSITY = 10;
-    private final int PANEL_DENSITY_DEFAULT  = 10;
+    private final int PANEL_DENSITY_DEFAULT = 10;
     //刻度表点半径
     private int PANEL_POINT_RADIUS = 3;
-    private final int PANEL_POINT_RADIUS_DEFAULT  = 3;
+    private final int PANEL_POINT_RADIUS_DEFAULT = 3;
     //仪表盘进度
     private float PANEL_PROGRESS = 50;
-    private final int PANEL_PROGRESS_DEFAULT  = 50;
+    private final int PANEL_PROGRESS_DEFAULT = 50;
     //仪表盘总进度
     private float PANEL_MAX = 100;
-    private final int PANEL_MAX_DEFAULT  = 100;
+    private final int PANEL_MAX_DEFAULT = 100;
     //指针大小
     private int INDICATOR_RADIUS = 10;
-    private final int INDICATOR_RADIUS_DEFAULT  = 10;
+    private final int INDICATOR_RADIUS_DEFAULT = 10;
+    //是否可以触摸控制
+    private boolean TOUCHABLE = true;
+    private final boolean TOUCHABLE_DEDAULT = true;
 
     public DriverProgress(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public DriverProgress(Context context, AttributeSet attrs) {
@@ -57,8 +61,11 @@ public class DriverProgress extends View{
         super(context, attrs, defStyleAttr);
         this.context = context;
         this.paint = new Paint();
-        initAttr(attrs,defStyleAttr);
-        initEvent();
+        initAttr(attrs, defStyleAttr);
+
+        if (TOUCHABLE) {
+            initEvent();
+        }
     }
 
     private void initEvent() {
@@ -72,12 +79,12 @@ public class DriverProgress extends View{
                 float y = event.getY();
 
                 float yh = iy - y;//角对边
-                float progress = 0;
+                float progress;
                 //sin
                 float sin = yh / (PANEL_RADIUS + PANEL_WIDTH); //sin = 角对边 / 半径
-                if(x < ix){//度数<90 Math.asin 反三角函数
+                if (x < ix) {//度数<90 Math.asin 反三角函数
                     progress = (float) ((float) Math.asin(sin) * 2 / Math.PI * PANEL_MAX);
-                }else{ //度数>90
+                } else { //度数>90
                     progress = (float) (PANEL_MAX - (float) Math.asin(sin) * 2 / Math.PI * PANEL_MAX);
                 }
 
@@ -90,30 +97,24 @@ public class DriverProgress extends View{
     private void initAttr(AttributeSet attrs, int defStyleAttr) {
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DriverProgress, defStyleAttr, 0);
         int n = a.getIndexCount();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int attr = a.getIndex(i);
             if (attr == R.styleable.DriverProgress_panel_radius) {
                 PANEL_RADIUS = a.getDimensionPixelSize(attr, DensityUtils.dp2px(getContext(), PANEL_RADIUS_DEFALUT));
-
             } else if (attr == R.styleable.DriverProgress_panel_width) {
                 PANEL_WIDTH = a.getDimensionPixelSize(attr, DensityUtils.dp2px(getContext(), PANEL_WIDTH_DEFAULT));
-
             } else if (attr == R.styleable.DriverProgress_panel_density) {
                 PANEL_DENSITY = a.getInt(attr, PANEL_DENSITY_DEFAULT);
-
             } else if (attr == R.styleable.DriverProgress_panel_point_radius) {
                 PANEL_POINT_RADIUS = a.getDimensionPixelSize(attr, DensityUtils.dp2px(getContext(), PANEL_POINT_RADIUS_DEFAULT));
-
             } else if (attr == R.styleable.DriverProgress_panel_progress) {
                 PANEL_PROGRESS = a.getInt(attr, PANEL_PROGRESS_DEFAULT);
-
             } else if (attr == R.styleable.DriverProgress_panel_max) {
                 PANEL_MAX = a.getInt(attr, PANEL_MAX_DEFAULT);
-
             } else if (attr == R.styleable.DriverProgress_indicator_radius) {
                 INDICATOR_RADIUS = a.getDimensionPixelOffset(attr, DensityUtils.dp2px(getContext(), INDICATOR_RADIUS_DEFAULT));
-
+            } else if (attr == R.styleable.DriverProgress_touchable) {
+                TOUCHABLE = a.getBoolean(attr, TOUCHABLE_DEDAULT);
             }
 
         }
@@ -121,33 +122,26 @@ public class DriverProgress extends View{
     }
 
 
+
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-    {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int width;
-        int height ;
-        if (widthMode == MeasureSpec.EXACTLY)
-        {
+        int height;
+        if (widthMode == MeasureSpec.EXACTLY) {
             width = widthSize;
-        } else
-        {
-            int desired = (int) (getPaddingLeft() + 2*PANEL_RADIUS+1.5f*PANEL_WIDTH + getPaddingRight());
-            width = desired;
+        } else {
+            width = (int) (getPaddingLeft() + 2 * PANEL_RADIUS + 1.5f * PANEL_WIDTH + getPaddingRight());
         }
 
-        if (heightMode == MeasureSpec.EXACTLY)
-        {
+        if (heightMode == MeasureSpec.EXACTLY) {
             height = heightSize;
-        } else
-        {
-            int desired = (int) (getPaddingTop() + PANEL_RADIUS+PANEL_WIDTH + getPaddingBottom());
-            height = desired;
+        } else {
+            height = getPaddingTop() + PANEL_RADIUS + PANEL_WIDTH + getPaddingBottom();
         }
-
 
 
         setMeasuredDimension(width, height);
@@ -157,92 +151,72 @@ public class DriverProgress extends View{
     /**
      * 仪表盘：白色圆环+蓝色圆弧+一组圆点（sin，cos）
      * 指针：圆形+三角形+小圆点
-     * @param canvas
      */
     @Override
     protected void onDraw(Canvas canvas) {
-        this.paint.setColor(getResources().getColor(R.color.panel_bottom_white));
+        this.paint.setColor(ContextCompat.getColor(context, R.color.panel_bottom_white));
         this.paint.setAntiAlias(true); //消除锯齿
         this.paint.setStyle(Paint.Style.STROKE); //绘制空心圆
         /**
          * 绘制仪表盘底色
          */
-        /*float x = (getWidth() - getHeight() / 2) / 2;
-        float y = getHeight() / 4;*/
-
-        float x = PANEL_WIDTH/2;
-        float y = PANEL_WIDTH/2;
-
-        //float radius = ((getWidth() - x) - x) / 2;//半径 = (endx-startx) / 2
-
-        /*RectF oval = new RectF( x, y,
-                getWidth() - x, getHeight() - y);*/
-        RectF oval = new RectF( x, y,
-                PANEL_WIDTH+2*PANEL_RADIUS, 2*PANEL_RADIUS+PANEL_WIDTH);
-
+        float x = PANEL_WIDTH / 2;
+        float y = PANEL_WIDTH / 2;
+        RectF oval = new RectF(x, y,
+                PANEL_WIDTH + 2 * PANEL_RADIUS, 2 * PANEL_RADIUS + PANEL_WIDTH);
         this.paint.setStrokeWidth(PANEL_WIDTH);
         canvas.drawArc(oval, 180, 180, false, paint);
-        //canvas.drawRect(oval,paint);
         /**
          * 绘制仪表盘进度
          */
-        this.paint.setColor(getResources().getColor(R.color.panel_progress_blue));
-        this.paint.setAntiAlias(true); //消除锯齿
-        this.paint.setStyle(Paint.Style.STROKE); //绘制空心圆
-
-        this.paint.setStrokeWidth(PANEL_WIDTH);
-
-        int sweepAngle = (int) (PANEL_PROGRESS/PANEL_MAX * 180.0f);
-
+        this.paint.setColor(ContextCompat.getColor(context, R.color.panel_progress_blue));
+        int sweepAngle = (int) (PANEL_PROGRESS / PANEL_MAX * 180.0f);
         canvas.drawArc(oval, 180, sweepAngle, false, paint);
 
 
         /**
          * 绘制仪表盘刻度
          */
-        paint.setColor(getResources().getColor(R.color.white));
+        paint.setColor(ContextCompat.getColor(context, R.color.white));
         paint.setStyle(Paint.Style.FILL);
         paint.setStrokeWidth(PANEL_POINT_RADIUS);
 
-        float startx = x ;
-        float starty = PANEL_RADIUS+PANEL_WIDTH*0.75f-PANEL_POINT_RADIUS;
+        float startx = x;
+        float starty = PANEL_RADIUS + PANEL_WIDTH * 0.75f - PANEL_POINT_RADIUS;
 
-        float radius = PANEL_RADIUS+PANEL_WIDTH/4;
-        //canvas.drawCircle(x,starty,5, paint);
+        float radius = PANEL_RADIUS + PANEL_WIDTH / 4;
         //点的密度
         int density = 180 / PANEL_DENSITY;
         float potsx[] = new float[PANEL_DENSITY];
         float potsy[] = new float[PANEL_DENSITY];
-        for(int i = 1 ; i <PANEL_DENSITY;i++){
-            potsx[i] = (float) (startx + (radius*(1 - Math.cos(density*i*Math.PI/180))));
-            potsy[i] = (float) (starty -  radius*Math.sin(density*i*Math.PI/180) + PANEL_POINT_RADIUS);
-            canvas.drawCircle(potsx[i],potsy[i],PANEL_POINT_RADIUS, paint);
+        for (int i = 1; i < PANEL_DENSITY; i++) {
+            potsx[i] = (float) (startx + (radius * (1 - Math.cos(density * i * Math.PI / 180))));
+            potsy[i] = (float) (starty - radius * Math.sin(density * i * Math.PI / 180) + PANEL_POINT_RADIUS);
+            canvas.drawCircle(potsx[i], potsy[i], PANEL_POINT_RADIUS, paint);
         }
 
 
         /**
          * 绘制底圆
          */
-        this.paint.setColor(getResources().getColor(R.color.panel_indicator_color));
-        this.paint.setAntiAlias(true); //消除锯齿
-        paint.setStyle(Paint.Style.FILL);
-        float ix = PANEL_RADIUS+PANEL_WIDTH*0.75f;
-        float iy = PANEL_RADIUS+PANEL_WIDTH*0.75f-PANEL_POINT_RADIUS;
-        canvas.drawCircle(ix,iy,INDICATOR_RADIUS, paint);
+        this.paint.setColor(ContextCompat.getColor(context, R.color.panel_indicator_color));
+        //圆点
+        float ix = PANEL_RADIUS + PANEL_WIDTH * 0.75f;
+        float iy = PANEL_RADIUS + PANEL_WIDTH * 0.75f - PANEL_POINT_RADIUS;
+        canvas.drawCircle(ix, iy, INDICATOR_RADIUS, paint);
 
         /**
          * 绘制针头  三角形
          */
-        float angle = (float) (PANEL_PROGRESS/PANEL_MAX*Math.PI);
-        float px = (float) (ix-(4*INDICATOR_RADIUS*Math.cos(angle)));
-        float py = (float) (iy-(4*INDICATOR_RADIUS*Math.sin(angle)));
+        float angle = (float) (PANEL_PROGRESS / PANEL_MAX * Math.PI);
+        float px = (float) (ix - (4 * INDICATOR_RADIUS * Math.cos(angle)));
+        float py = (float) (iy - (4 * INDICATOR_RADIUS * Math.sin(angle)));
         Path path = new Path();
-        //path.moveTo(ix-INDICATOR_RADIUS, iy-3);// 此点为多边形的起点
-        //.lineTo(ix+INDICATOR_RADIUS, iy-3);
-        //path.lineTo(ix, iy-4*INDICATOR_RADIUS);
-        path.moveTo((float) (ix-INDICATOR_RADIUS*Math.sin(angle)),(float) ((iy+INDICATOR_RADIUS*Math.cos(angle))));
-        path.lineTo((float) (ix+INDICATOR_RADIUS*Math.sin(angle)),(float) ((iy-INDICATOR_RADIUS*Math.cos(angle))));
-        path.lineTo(px,py);
+        path.moveTo((float) (ix - INDICATOR_RADIUS * Math.sin(angle)),
+                (float) ((iy + INDICATOR_RADIUS * Math.cos(angle))));// 此点为多边形的起点
+        path.lineTo((float) (ix + INDICATOR_RADIUS * Math.sin(angle)),
+                (float) ((iy - INDICATOR_RADIUS * Math.cos(angle))));
+        path.lineTo(px, py);
         path.close(); // 使这些点构成封闭的多边形
         canvas.drawPath(path, paint);
 
@@ -250,11 +224,26 @@ public class DriverProgress extends View{
          * 绘制轴  白色的点
          */
 
-        this.paint.setColor(getResources().getColor(R.color.white));
-        this.paint.setAntiAlias(true); //消除锯齿
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(ix,iy,INDICATOR_RADIUS/3, paint);
+        this.paint.setColor(ContextCompat.getColor(context, R.color.white));
+        canvas.drawCircle(ix, iy, INDICATOR_RADIUS / 3, paint);
 
+       /* *//**
+         * 绘制波浪
+         *//*
+        this.paint.setColor(getResources().getColor(R.color.panel_progress_blue));
+        paint.setStyle(Paint.Style.FILL);
+        Path pathquad = new Path();
+        float mradius = PANEL_RADIUS+PANEL_WIDTH*0.72f;
+        float qx_start = (float) (ix - mradius * Math.cos((sweepAngle - 2)/180.0f*Math.PI));
+        float qy_start = (float) (iy - mradius * Math.sin((sweepAngle - 2)/180.0f*Math.PI));
+        float qx_end1 = (float) (ix - (mradius - PANEL_WIDTH/2) * Math.cos((sweepAngle - 2)/180.0f*Math.PI));
+        float qy_end1 = (float) (iy - (mradius - PANEL_WIDTH/2) * Math.sin((sweepAngle - 2)/180.0f*Math.PI));
+        float qx_end2 = (float) (ix - (PANEL_RADIUS-PANEL_WIDTH*0.25f) * Math.cos((sweepAngle - 5)/180.0f*Math.PI));
+        float qy_end2 = (float) (iy - (PANEL_RADIUS-PANEL_WIDTH*0.25f) * Math.sin((sweepAngle - 5)/180.0f*Math.PI));
+        pathquad.moveTo(qx_start,qy_start);
+        pathquad.quadTo(qx_start+25,qy_start-25,qx_end1,qy_end1);
+        pathquad.quadTo(qx_end1+25,qy_end1-25,qx_end2,qy_end2);
+        canvas.drawPath(pathquad,paint);*/
         super.onDraw(canvas);
     }
 
@@ -265,12 +254,59 @@ public class DriverProgress extends View{
         this.PANEL_PROGRESS = progress;
         invalidate();
     }
+
+    /**
+     * 设置仪表盘宽度
+     */
+    public void setPanelWidth(int PANEL_WIDTH) {
+        this.PANEL_WIDTH = PANEL_WIDTH;
+        invalidate();
+    }
+
+    /**
+     * 设置半径
+     */
+    public void setPanelRadius(int PANEL_RADIUS) {
+        this.PANEL_RADIUS = PANEL_RADIUS;
+        invalidate();
+    }
+
+    /**
+     * 设置指针长度
+     */
+    public void setIndicatorRadius(int INDICATOR_RADIUS) {
+        this.INDICATOR_RADIUS = INDICATOR_RADIUS;
+        invalidate();
+    }
+
+    /**
+     * 设置精度
+     */
+    public void setPanelDensity(int PANEL_DENSITY) {
+        if (PANEL_DENSITY > 0) {
+            this.PANEL_DENSITY = PANEL_DENSITY;
+            invalidate();
+        }
+    }
+
+    /**
+     * 重置
+     */
+    public void resetView() {
+        this.PANEL_PROGRESS = PANEL_PROGRESS_DEFAULT;
+        this.PANEL_WIDTH = PANEL_WIDTH_DEFAULT;
+        this.INDICATOR_RADIUS = INDICATOR_RADIUS_DEFAULT;
+        this.PANEL_DENSITY = PANEL_DENSITY_DEFAULT;
+        this.PANEL_RADIUS = PANEL_RADIUS_DEFALUT;
+        invalidate();
+    }
+
     /**
      * 启动动画
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void startAnimation(){
-        ValueAnimator animator = ValueAnimator.ofFloat(0,PANEL_PROGRESS);
+    public void startAnimation() {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, PANEL_PROGRESS);
         animator.setDuration(3000);
 
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
